b27b83e7348bf3cd4590d3c50f4a4d80ccd564da
Rupert Carmichael <5050061-carmiker@users.noreply.gitlab.com>
2024-02-04 17:08:52 -0500

    jni: Add jni directory

---
+++ libretro/jni/Android.mk
@@ -0,0 +1,21 @@
+LOCAL_PATH := $(call my-dir)
+
+ROOT_DIR := $(LOCAL_PATH)/..
+CORE_DIR := $(ROOT_DIR)/src
+
+include $(ROOT_DIR)/Makefile.common
+
+COREFLAGS := -DANDROID -D__LIBRETRO__ $(INCFLAGS)
+
+GIT_VERSION := " $(shell git rev-parse --short HEAD || echo unknown)"
+ifneq ($(GIT_VERSION)," unknown")
+  COREFLAGS += -DGIT_VERSION=\"$(GIT_VERSION)\"
+endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := retro
+LOCAL_SRC_FILES := $(SOURCES_C)
+LOCAL_CFLAGS    := $(COREFLAGS)
+LOCAL_LDFLAGS   := -Wl,-version-script=$(ROOT_DIR)/link.T
+LOCAL_LDLIBS    := -lz
+include $(BUILD_SHARED_LIBRARY)
